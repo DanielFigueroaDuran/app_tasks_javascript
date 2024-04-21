ca0de84382b2f98836eadd0c404b21f03e8bb962
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-04-21 21:20:10 +0200

    render tasks

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 const modalForm = document.querySelector("#modal__form");
 const btnAddTask = document.querySelector("#add-task");
 const btnCloseModal = document.querySelector("#close-modal");
-const formTask = document.querySelector("#form-task");
+const taskContainer = document.querySelector("#task-container");
 
-// console.log(form);
+const formTask = document.querySelector("#form-task");
+const taskInput = document.querySelector("#task-input");
+const templateTask = document.querySelector("#template-task");
+
+//console.log(taskContainer);
 
 // Open Modal //
 
@@ -38,4 +42,45 @@ function closeModal() {
 
 formTask.addEventListener("submit", function (event) {
   event.preventDefault();
+  const currentValueInput = taskInput.value.trim();
+  //console.log(currentValueInput);
+
+  if (currentValueInput.length > 3) {
+    const newTask = createTask(currentValueInput);
+    //console.log(newTask);
+    renderTask(newTask);
+
+    formTask.reset();
+    closeModal();
+  }
 });
+
+// create Tasks //
+
+function createTask(tasName) {
+  return {
+    name: tasName,
+    complete: false,
+    id: new Date().getTime(),
+  };
+}
+
+//  Render Tasks //
+
+function renderTask(task) {
+  const { name, complete: state, id } = task;
+  const taskTemplate = templateTask.content.cloneNode(true);
+
+  if (state) {
+    taskTemplate.querySelector(".tasks__item").dataset.state = "complete";
+    taskTemplate.querySelector(".tasks__checked").checked = true;
+  } else {
+    taskTemplate.querySelector(".tasks__item").dataset.state = "incomplete";
+    taskTemplate.querySelector(".tasks__name").contentEditable = true;
+  }
+
+  taskTemplate.querySelector(".tasks__name").textContent = name;
+  taskTemplate.querySelector(".tasks__item").id = id;
+
+  taskContainer.prepend(taskTemplate);
+}
