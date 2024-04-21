d07bd49bb47f63ae0fa9a12f0a9a99a702b5763c
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-04-21 19:17:27 +0200

    add modal js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
 const modalForm = document.querySelector("#modal__form");
+const btnAddTask = document.querySelector("#add-task");
+const btnCloseModal = document.querySelector("#close-modal");
+const formTask = document.querySelector("#form-task");
 
-modalForm.showModal();
+// console.log(form);
+
+// Open Modal //
+
+btnAddTask.addEventListener("click", () => {
+  modalForm.showModal();
+  modalForm.querySelector("input").focus();
+});
+
+// Close Mosal Event //
+
+btnCloseModal.addEventListener("click", closeModal);
+
+//Close Modal //
+
+function closeModal() {
+  //console.log(modalForm);
+  modalForm.style.animation = "fade .3s forwards";
+
+  modalForm.addEventListener(
+    "animationend",
+    function (e) {
+      modalForm.style.animation = "";
+      modalForm.close();
+    },
+    {
+      once: true,
+    }
+  );
+}
+
+// Add Tasks//
+
+formTask.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
